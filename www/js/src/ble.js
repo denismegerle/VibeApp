a6e85234308ca019789c6ef5b3e6298651924454
Denis Megerle <megerle.denis@gmail.com>
2018-02-07 19:20:57 +0100

    added basic ble functionality

--- www/js/src/ble.js
+++ www/js/src/ble.js
@@ -1,3 +1,9 @@
+var VIB_SERVICE	       	= "713D0000-503E-4C75-BA94-3148F18D941E";
+var VIB_CHARACTERISTIC	= "713D0003-503E-4C75-BA94-3148F18D941E";
+
+var data = new Uint8Array(5);
+var vibrate = new Uint8Array(5);
+
 function createBLListElement(device) {
 	var li = document.createElement("li");
 	
@@ -27,10 +33,71 @@ function createBLListElement(device) {
 }
 
 function bleVibrate() {
+	prepareData(10);
+	alert(data[0] + " " + data[1] + " " + data[2] + " " + data[3] + " " + data[4]);
 	ble.isConnected(bleDevice.id, function() {
 		// connected, send vibration signals
-		sendVibration();
+		//updateDataBuffer();
+		
+		ble.writeWithoutResponse(bleDevice.id, VIB_SERVICE, VIB_CHARACTERISTIC, data.buffer, writeDone, writeFailure);
 	}, function() {
 		// not connected, do nothing
+		
 	});
+}
+
+function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
+
+// data[0] am ahorn, gegen uhrzeigersinn
+function updateDataBuffer() {
+	if (userGoDir <= 0)
+		return;
+	
+	data[0] = 0x00;
+	data[1] = 0xff;
+	data[2] = 0x00;
+	data[3] = 0x00;
+	data[4] = 0x00;
+	
+	var pd = projectDir(0);
+	
+	// links ist 318, im Uhrzeigersinn nehmen die Zahlen zu, ab und zu minus!
+}
+
+function prepareData(dir) {
+	vibrate[0] = (dir > getBoundary(0, -1) || dir < getBoundary(0, 1)) ? 1 : 0;
+	for (var i = 1; i < 5; i++) {
+		var leftBoundary = getBoundary(i, -1);
+		var rightBoundary = getBoundary(i, 1);
+		vibrate[i] = (dir > leftBoundary && dir < rightBoundary) ? 1 : 0;
+	}
+	
+	distanceScale = 1.0;
+	
+	if (isNumber(nextWaypoint.dist))
+		distanceScale = getDistanceScale(nextWaypoint.dist);
+	
+	for (var i = 0; i < 5; i++) {
+		data[i] = vibrate[i] * distanceScale * 0xff;
+	}
+}
+
+function getDistanceScale(distance) {
+	return 2.0;
+}
+
+function getBoundary(positionNumber, side) {
+	return (side == -1) ? mod(positionNumber * 72 - (2 / 3) * 72, 360) : mod(positionNumber * 72 + (2 / 3) * 72, 360);
+}
+
+function mod(a, b) {
+	return ((a%b)+b)%b;
+}
+
+//Callback when write is done.
+function writeDone() {
+}
+
+// Callback when write fails.
+function writeFailure() {
 }
