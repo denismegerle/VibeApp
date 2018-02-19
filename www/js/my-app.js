2e8750fcc1ef3e2b666d153d592f5cb9fc6553fb
Denis Megerle <megerle.denis@gmail.com>
2018-02-19 01:46:36 +0100

    gmaps added, compass tab close to done, missing textinput field and navigate/route button

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,29 +1,41 @@
 // Initialize app
-var app = new Framework7({
-	material: true,
-	swipePanel: 'left'	// TODO fix this...
-});
-
+var app = new Framework7();
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
+var mainView = app.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
 
+var map
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+    initMap();
 });
 
+function debug() {
+	app.alert("hey");
+}
+
+function initMap() {
+	var uluru = {
+		lat : -25.363,
+		lng : 131.044
+	};
+	map = new google.maps.Map(document.getElementById('gmap'), {
+        zoom: 4,
+        center: uluru
+      });
+}
 
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
+app.onPageInit('about', function (page) {
     // Do something here for "about" page
 
 })
@@ -35,12 +47,12 @@ $$(document).on('pageInit', function (e) {
 
     if (page.name === 'about') {
         // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
+        app.alert('Here comes About page');
     }
 })
 
 // Option 2. Using live 'pageInit' event handlers for each page
 $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
+    app.alert('Here comes About page');
 })
